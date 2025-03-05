f08e6ad2743178432ab69625c9c7c99e56f3afcc
Dale Nil <offpath@gmail.com>
2025-03-04 22:07:23 -0800

    Reformat the logic programming language to be much simpler to parse and setup the structure of the AST.

--- internal/puzzle/logic.go
+++ internal/puzzle/logic.go
@@ -1,6 +1,9 @@
 package puzzle
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // Example:
 //
@@ -8,10 +11,10 @@ import "strings"
 // Dish:cashew tofu,lemon snapper,smoked pork,turkey soup
 // Score:42,49,56,63
 //
-// Dish(Sonia) = cashew tofu
-// Score(Karl) = Chef(smoked pork) + 7
-// Score(Freda) = 49
-// Score(Chef(turkey soup)) = Score(Sonia) - 7
+// eq(Dish(Sonia), cashew tofu)
+// eq(Score(Karl), plus(Chef(smoked pork), 7))
+// eq(Score(Freda, 49))
+// eq(Score(Chef(turkey soup)), minus(Score(Sonia), 7))
 //
 // Answers:
 // 42, Wade, lemon snapper
@@ -27,6 +30,140 @@ type category struct {
 type LogicPuzzle struct {
 	*Puzzle
 	categories map[string]*category
+}
+
+type astNode interface {
+	Evaluate() valueSet
+	TypeCheck() bool
+	Type() string
+}
+
+type valueSet map[string]bool
+
+type val struct {
+	v string
+	t string
+}
+
+func (v val) String() string {
+	return fmt.Sprintf("%s:%s", v.t, v.v)
+}
+
+func (v val) Evaluate() valueSet {
+	return map[string]bool{v.v: true}
+}
+
+func (v val) Type() string {
+	return v.t
+}
+
+func (v val) TypeCheck() bool {
+	return true
+}
+
+// TODO(dneal): Functions to support:
+// eq
+// neq
+// or
+// gt
+// gte
+// lt
+// lte
+// <category name>
+// plus
+// minus
+
+type equality struct {
+	left  astNode
+	right astNode
+	equal bool
+}
+
+func (e equality) Evalate() valueSet {
+	// TODO(dneal)
+	return nil
+}
+
+func (e equality) TypeCheck() bool {
+	return e.left.Type() == e.right.Type()
+}
+
+func (e equality) Type() string {
+	return "bool"
+}
+
+type or struct {
+	left  astNode
+	right astNode
+}
+
+func (o or) Evalate() valueSet {
+	// TODO(dneal)
+	return nil
+}
+
+func (o or) TypeCheck() bool {
+	return o.left.Type() == "bool" && o.right.Type() == "bool"
+}
+
+func (o or) Type() string {
+	return "bool"
+}
+
+type comparison struct {
+	left  astNode
+	right astNode
+	op    string
+}
+
+func (c comparison) Evalate() valueSet {
+	// TODO(dneal)
+	return nil
+}
+
+func (c comparison) TypeCheck() bool {
+	return c.left.Type() == c.right.Type()
+}
+
+func (c comparison) Type() string {
+	return "bool"
+}
+
+type plusMinus struct {
+	left  astNode
+	right astNode
+	op    string
+}
+
+func (p plusMinus) Evalate() valueSet {
+	// TODO(dneal)
+	return nil
+}
+
+func (p plusMinus) TypeCheck() bool {
+	return p.left.Type() == p.right.Type()
+}
+
+func (p plusMinus) Type() string {
+	return p.left.Type()
+}
+
+type connection struct {
+	name string
+	arg  astNode
+}
+
+func (c connection) Evalate() valueSet {
+	// TODO(dneal)
+	return nil
+}
+
+func (c connection) TypeCheck() bool {
+	return c.arg.Type() != "bool" && c.arg.Type() != "int" && c.arg.Type() != c.Type()
+}
+
+func (c connection) Type() string {
+	return c.name
 }
 
 func NewLogicPuzzle(s string) *LogicPuzzle {
