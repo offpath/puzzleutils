49f649672a377cc025f690c2a9a9131af99d2f42
Dale Nil <offpath@gmail.com>
2025-03-05 23:05:16 -0800

    Initial implementation of a parser for logic rules.

--- internal/puzzle/logic.go
+++ internal/puzzle/logic.go
@@ -2,6 +2,8 @@ package puzzle
 
 import (
 	"fmt"
+	"log"
+	"regexp"
 	"strings"
 )
 
@@ -12,15 +14,27 @@ import (
 // Score:42,49,56,63
 //
 // eq(Dish(Sonia), cashew tofu)
-// eq(Score(Karl), plus(Chef(smoked pork), 7))
+// eq(Score(Karl), plus(Chef(smoked pork), 1))
 // eq(Score(Freda, 49))
-// eq(Score(Chef(turkey soup)), minus(Score(Sonia), 7))
+// eq(Score(Chef(turkey soup)), minus(Score(Sonia), 1 ))
 //
 // Answers:
 // 42, Wade, lemon snapper
 // 49, Freda, smoked pork
 // 56, Karl, turkey soup
 // 63, Sonia, cashew tofu
+//
+// Supported functions:
+// eq
+// neq
+// or
+// gt
+// gte
+// lt
+// lte
+// plus
+// minus
+// <category name>
 
 type category struct {
 	name   string
@@ -30,10 +44,12 @@ type category struct {
 type LogicPuzzle struct {
 	*Puzzle
 	categories map[string]*category
+	values     map[string]*val
+	rules      []astNode
 }
 
 type astNode interface {
-	Evaluate() valueSet
+	Evaluate(lp *LogicPuzzle) valueSet
 	TypeCheck() bool
 	Type() string
 }
@@ -41,37 +57,26 @@ type astNode interface {
 type valueSet map[string]bool
 
 type val struct {
-	v string
-	t string
+	v        string
+	category string
+	index    int
 }
 
 func (v val) String() string {
-	return fmt.Sprintf("%s:%s", v.t, v.v)
+	return fmt.Sprintf("%s:%s(%d)", v.category, v.v, v.index)
 }
 
-func (v val) Evaluate() valueSet {
+func (v val) Evaluate(lp *LogicPuzzle) valueSet {
 	return map[string]bool{v.v: true}
 }
 
 func (v val) Type() string {
-	return v.t
+	return v.category
 }
 
 func (v val) TypeCheck() bool {
 	return true
 }
-
-// TODO(dneal): Functions to support:
-// eq
-// neq
-// or
-// gt
-// gte
-// lt
-// lte
-// <category name>
-// plus
-// minus
 
 type equality struct {
 	left  astNode
@@ -79,8 +84,8 @@ type equality struct {
 	equal bool
 }
 
-func (e equality) Evalate() valueSet {
-	// TODO(dneal)
+func (e equality) Evaluate(lp *LogicPuzzle) valueSet {
+	// TODO(dneal): returns true if any overlap between value sets
 	return nil
 }
 
@@ -97,7 +102,7 @@ type or struct {
 	right astNode
 }
 
-func (o or) Evalate() valueSet {
+func (o or) Evaluate(lp *LogicPuzzle) valueSet {
 	// TODO(dneal)
 	return nil
 }
@@ -116,7 +121,7 @@ type comparison struct {
 	op    string
 }
 
-func (c comparison) Evalate() valueSet {
+func (c comparison) Evaluate(lp *LogicPuzzle) valueSet {
 	// TODO(dneal)
 	return nil
 }
@@ -135,7 +140,7 @@ type plusMinus struct {
 	op    string
 }
 
-func (p plusMinus) Evalate() valueSet {
+func (p plusMinus) Evaluate(lp *LogicPuzzle) valueSet {
 	// TODO(dneal)
 	return nil
 }
@@ -153,7 +158,7 @@ type connection struct {
 	arg  astNode
 }
 
-func (c connection) Evalate() valueSet {
+func (c connection) Evaluate(lp *LogicPuzzle) valueSet {
 	// TODO(dneal)
 	return nil
 }
@@ -166,15 +171,95 @@ func (c connection) Type() string {
 	return c.name
 }
 
+func (lp *LogicPuzzle) parseExpression(tokens []string, i *int) astNode {
+	token := strings.TrimSpace(tokens[*i])
+	*i++
+	var args []astNode
+	if token == "(" {
+		*i++
+		for {
+			args = append(args, lp.parseExpression(tokens, i))
+			next := tokens[*i]
+			*i++
+			if next == ")" {
+				break
+			}
+			if next != "," {
+				log.Fatalf("Expected , or )")
+			}
+		}
+	}
+
+	if len(args) == 0 {
+		v := lp.values[token]
+		if v == nil {
+			log.Fatalf("Unknown value: %s", token)
+		}
+		return v
+	}
+
+	if lp.categories[token] != nil {
+		if len(args) != 1 {
+			log.Fatalf("Expected 1 argument for %s", token)
+		}
+		return connection{token, args[0]}
+	}
+
+	if len(args) != 2 {
+		log.Fatalf("Expected 2 arguments for %s", token)
+	}
+
+	switch token {
+	case "eq":
+		fallthrough
+	case "neq":
+		return equality{args[0], args[1], token == "eq"}
+	case "or":
+		return or{args[0], args[1]}
+	case "gt":
+		fallthrough
+	case "gte":
+		fallthrough
+	case "lt":
+		fallthrough
+	case "lte":
+		return comparison{args[0], args[1], token}
+	case "plus":
+		fallthrough
+	case "minus":
+		return plusMinus{args[0], args[1], token}
+	}
+
+	log.Fatalf("Unknown expression: %s", token)
+	return nil
+}
+
+func (lp *LogicPuzzle) parseRule(s string) {
+	re := regexp.MustCompile("[(,)]|[^(,)]+")
+	tokens := re.FindAllString(s, -1)
+	i := 0
+	astNode := lp.parseExpression(tokens, &i)
+	if !astNode.TypeCheck() || astNode.Type() != "bool" {
+		log.Fatalf("Invalid rule: %s", s)
+	}
+	lp.rules = append(lp.rules, astNode)
+}
+
 func NewLogicPuzzle(s string) *LogicPuzzle {
 	lines := strings.Split(s, "\n")
 	result := &LogicPuzzle{}
-	for _, line := range lines {
+	i := 0
+	for ; i < len(lines); i++ {
+		line := lines[i]
 		if line == "" {
 			break
 		}
 		parts := strings.Split(line, ":")
 		result.categories[parts[0]] = &category{parts[0], strings.Split(parts[1], ",")}
+	}
+	i++
+	for ; i < len(lines); i++ {
+		result.parseRule(lines[i])
 	}
 	return result
 }
