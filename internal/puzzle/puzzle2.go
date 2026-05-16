c86ae9bb815e59fd4aac0520cc572e976ee8771b
Dale Nil <offpath@gmail.com>
2026-05-16 13:26:51 -0700

    Make it so that you can mark Variables as equal and they'll share the same csp.Decision under the hood.

--- internal/puzzle/puzzle2.go
+++ internal/puzzle/puzzle2.go
@@ -49,30 +49,50 @@ type Variable struct {
 	possibleValues ValueSet
 	currentValues  ValueSet
 	id             int
+	// rep is the parent node in the disjoint-set of equal variables.
+	// Variables that are marked equal will share the same root representative.
+	rep *Variable
+}
+
+func (v *Variable) root() *Variable {
+	root := v
+	for root.rep != root {
+		root = root.rep
+	}
+	return root
+}
+
+// MarkEqual marks the variable as equal to all of the provided variables.
+func (v *Variable) MarkEqual(vars ...*Variable) {
+	v.p.MarkEqual(append([]*Variable{v}, vars...)...)
 }
 
 func (v *Variable) SetValueRange(vs ValueSet) {
+	v = v.root()
 	v.possibleValues = vs
 	v.currentValues = vs.Clone()
 }
 
 func (v *Variable) SetInitialValue(val *Value) {
+	v = v.root()
 	v.possibleValues = ValueSet{val: true}
 	v.currentValues = ValueSet{val: true}
 }
 
 func (v *Variable) EliminateValue(val *Value) {
+	v = v.root()
 	delete(v.currentValues, val)
 	v.p.problem.Get(v.id).Restrict(val.id)
 }
 
 func (v *Variable) RestrictTo(val *Value) {
+	v = v.root()
 	v.currentValues = ValueSet{val: true}
 	v.p.problem.Get(v.id).RestrictTo(val.id)
 }
 
 func (v *Variable) Values() ValueSet {
-	return v.currentValues
+	return v.root().currentValues
 }
 
 type Constraint interface {
@@ -143,6 +163,7 @@ func (p *Puzzle2) NewVariable() *Variable {
 		currentValues:  ValueSet{},
 		id:             len(p.variables),
 	}
+	result.rep = result
 	p.variables = append(p.variables, result)
 	return result
 }
@@ -154,6 +175,30 @@ func (p *Puzzle2) AddConstraint(c Constraint) {
 	})
 }
 
+func (p *Puzzle2) MarkEqual(vars ...*Variable) {
+	if len(vars) < 2 {
+		return
+	}
+	first := vars[0].root()
+	for _, v := range vars[1:] {
+		rep := v.root()
+		if rep != first {
+			rep.rep = first
+
+			if len(first.possibleValues) == 0 {
+				first.possibleValues = rep.possibleValues.Clone()
+			} else if len(rep.possibleValues) > 0 {
+				for val := range first.possibleValues {
+					if !rep.possibleValues[val] {
+						delete(first.possibleValues, val)
+					}
+				}
+			}
+			first.currentValues = first.possibleValues.Clone()
+		}
+	}
+}
+
 type constraintShim struct {
 	c Constraint
 }
@@ -161,6 +206,7 @@ type constraintShim struct {
 func (c constraintShim) Init(all []*csp.Decision, size int) {}
 func (c constraintShim) Apply(all, dirty []*csp.Decision) bool {
 	for _, v := range c.c.Variables() {
+		v = v.root()
 		v.currentValues = v.possibleValues.Clone()
 		for val := range v.currentValues {
 			if !v.p.problem.Get(v.id).Possible(val.id) {
@@ -190,15 +236,32 @@ func (c valueSetConstraint) Apply(all, dirty []*csp.Decision) bool {
 }
 
 func (p *Puzzle2) Solve(settings csp.Settings) bool {
-	p.problem = csp.NewProblem(len(p.variables), len(p.values))
+	numDecisions := 0
 	for _, v := range p.variables {
-		group := []int{v.id}
-		p.problem.AddGroup(group, valueSetConstraint{v.possibleValues})
+		if v.rep == v {
+			v.id = numDecisions
+			numDecisions++
+		}
+	}
+	for _, v := range p.variables {
+		v.id = v.root().id
+	}
+
+	p.problem = csp.NewProblem(numDecisions, len(p.values))
+	for _, v := range p.variables {
+		if v.rep == v {
+			group := []int{v.id}
+			p.problem.AddGroup(group, valueSetConstraint{v.possibleValues})
+		}
 	}
 	for _, g := range p.groups {
 		var group []int
+		seen := map[int]bool{}
 		for _, v := range g.variables {
-			group = append(group, v.id)
+			if !seen[v.id] {
+				group = append(group, v.id)
+				seen[v.id] = true
+			}
 		}
 		p.problem.AddGroup(group, constraintShim{g.c})
 	}
